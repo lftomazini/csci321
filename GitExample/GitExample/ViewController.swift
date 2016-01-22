ddc460df4154740ed6f0b6124acc671609f782c1
Luís Felipe <lftomazini@wifi177-038.bucknell.edu>
2016-01-22 12:31:50 -0500

    Hello World!

--- GitExample/GitExample/ViewController.swift
+++ GitExample/GitExample/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("Greetings from viewDiDLoad")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
