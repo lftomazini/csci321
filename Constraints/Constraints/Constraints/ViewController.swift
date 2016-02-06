e829dd6f39aca3ef36c4a548757c2894432b1d3d
Luís Felipe <lftomazini@wifi177-038.bucknell.edu>
2016-02-05 22:14:37 -0500

    Added left view and its constraints.

--- Constraints/Constraints/Constraints/ViewController.swift
+++ Constraints/Constraints/Constraints/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         view.backgroundColor = UIColor.whiteColor()
         let headlineView = addHeadlineView()
+        let leftView = addLeftView(alignedWidth: headlineView)
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -62,5 +63,41 @@ class ViewController: UIViewController {
             
             return headlineView
         }
+        
+        ///
+        /// Adds a light purple view on the left. Its width will be 0.3 of the
+        /// headline view, and its height will be variable, always extending to 20 
+        /// points of the bottom of the screen.
+        ///
+        /// - parameter headlineView: the view across the top of the screen.
+        ///
+        /// - returns: the left view
+        ///
+        func addLeftView(alignedWidth headlineView: UIView) -> UIView {
+            let leftView = UIView()
+            leftView.backgroundColor = UIColor(red: 0.725, green: 0.739, blue: 1.0, alpha: 1.0)
+            view.addSubview(leftView)
+            leftView.translatesAutoresizingMaskIntoConstraints = false
+            
+            // The left view has width of  30% of the headline view width.
+            let leftViewWidth = NSLayoutConstraint(item: leftView, attribute: .Width, relatedBy: .Equal, toItem: nil, attribute: .NotAnAttribute, multiplier: 0.3, constant: 200)
+            leftView.addConstraint(leftViewWidth)
+            
+            // The left view is always 20 point from the bottom.
+            let leftViewHeight = NSLayoutConstraint(item: leftView, attribute: .Bottom, relatedBy: .Equal, toItem: view, attribute: .Bottom, multiplier: 1.0, constant: -20.0)
+            view.addConstraint(leftViewHeight)
+            
+            // The left view has its top aligned with the bottom of the headline View
+            // with 20 points between them
+            let leftViewTop = NSLayoutConstraint(item: leftView, attribute: .Top, relatedBy: .Equal, toItem: headlineView, attribute: .Bottom, multiplier: 1.0, constant: 20.0)
+            view.addConstraint(leftViewTop)
+            
+            // On the left, the left view should be 20 points from the edge of the screen
+            let leftViewLeft = NSLayoutConstraint(item: leftView, attribute: .Left, relatedBy: .Equal, toItem: headlineView, attribute: .Left, multiplier: 1.0, constant: 0.0)
+            view.addConstraint(leftViewLeft)
+            
+            return leftView
+        }
+
     }
 
