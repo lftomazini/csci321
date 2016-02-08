95f313eee9763a099b40516a0e83feff5bdda474
Luís Felipe <lftomazini@wifi177-038.bucknell.edu>
2016-02-08 12:22:44 -0500

    Exercise constarints, complete

--- Constraints/Constraints/Constraints/ViewController.swift
+++ Constraints/Constraints/Constraints/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController {
         view.backgroundColor = UIColor.whiteColor()
         let headlineView = addHeadlineView()
         let leftView = addLeftView(alignedWidth: headlineView)
+        let centerredButton = addCenterredButton(alignedWidth: headlineView, alignedWith: leftView)
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -80,8 +81,8 @@ class ViewController: UIViewController {
             leftView.translatesAutoresizingMaskIntoConstraints = false
             
             // The left view has width of  30% of the headline view width.
-            let leftViewWidth = NSLayoutConstraint(item: leftView, attribute: .Width, relatedBy: .Equal, toItem: nil, attribute: .NotAnAttribute, multiplier: 0.3, constant: 200)
-            leftView.addConstraint(leftViewWidth)
+            let leftViewWidth = NSLayoutConstraint(item: leftView, attribute: .Width, relatedBy: .Equal, toItem: headlineView, attribute: .Width, multiplier: 0.3, constant: 0.0) //TO DO: change to headlineView.width
+            view.addConstraint(leftViewWidth)
             
             // The left view is always 20 point from the bottom.
             let leftViewHeight = NSLayoutConstraint(item: leftView, attribute: .Bottom, relatedBy: .Equal, toItem: view, attribute: .Bottom, multiplier: 1.0, constant: -20.0)
@@ -93,10 +94,62 @@ class ViewController: UIViewController {
             view.addConstraint(leftViewTop)
             
             // On the left, the left view should be 20 points from the edge of the screen
-            let leftViewLeft = NSLayoutConstraint(item: leftView, attribute: .Left, relatedBy: .Equal, toItem: headlineView, attribute: .Left, multiplier: 1.0, constant: 0.0)
+            let leftViewLeft = NSLayoutConstraint(item: leftView, attribute: .Left, relatedBy: .Equal, toItem: view, attribute: .Left, multiplier: 1.0, constant: 20.0)
             view.addConstraint(leftViewLeft)
             
+            print(headlineView.frame.width)
             return leftView
+        }
+        
+        ///
+        /// Adds a light  a button to centerred with the headline view and the left view
+        ///
+        /// - parameter headlineView: the view across the top of the screen.
+        ///
+        /// - parameter leftView: the view on the left under the headlineView
+        ///
+        /// - returns: the button view
+        ///
+        func addCenterredButton(alignedWidth headlineView: UIView, alignedWith leftView: UIView) -> UIView {
+            let centerredView = UIView()
+            //centerredView.backgroundColor = UIColor(red: 0.0, green: 1.0, blue: 0.0, alpha: 1.0)
+            view.addSubview(centerredView)
+            centerredView.translatesAutoresizingMaskIntoConstraints = false
+            
+            // On the left, the button view should be 0 points from the right edge of the left view
+            let buttonLeft = NSLayoutConstraint(item: centerredView, attribute: .Left, relatedBy: .Equal, toItem: leftView, attribute: .Right, multiplier: 1.0, constant: 0.0)
+            view.addConstraint(buttonLeft)
+
+            // The button view has its top aligned with the bottom of the headline View
+            // with 20 points between them
+            let buttonTop = NSLayoutConstraint(item: centerredView, attribute: .Top, relatedBy: .Equal, toItem: headlineView, attribute: .Bottom, multiplier: 1.0, constant: 20.0)
+            view.addConstraint(buttonTop)
+            
+            // The button view is always 20 point from the bottom.
+            let buttonBottom = NSLayoutConstraint(item: centerredView, attribute: .Bottom, relatedBy: .Equal, toItem: view, attribute: .Bottom, multiplier: 1.0, constant: -20.0)
+            view.addConstraint(buttonBottom)
+            
+            
+            // The button view should be 20 points from the right edge of the screen.
+            let buttonRight = NSLayoutConstraint(item: centerredView, attribute: .Right, relatedBy: .Equal, toItem: view, attribute: .Right, multiplier: 1.0, constant: -20.0)
+            view.addConstraint(buttonRight)
+            
+            // Creates the button
+            let centerredButton = UIButton()
+            centerredButton.setTitle("Tap me", forState: UIControlState.Normal)
+            centerredButton.setTitleColor(UIColor(red: 0.247, green: 0.635, blue: 1.0, alpha: 1.0), forState: UIControlState.Normal)
+            view.addSubview(centerredButton)
+            centerredButton.translatesAutoresizingMaskIntoConstraints = false
+            
+            // Centers the x coordinate of the button with the x coordinate of the canterred view
+            let xCoordinate = NSLayoutConstraint(item: centerredButton, attribute: .CenterX, relatedBy: .Equal, toItem: centerredView, attribute: .CenterX, multiplier: 1.0, constant: 0.0)
+            view.addConstraint(xCoordinate)
+            
+            // Centers the y coordinate of the button with the y coordinate of the canterred view
+            let yCoordinate = NSLayoutConstraint(item: centerredButton, attribute: .CenterY, relatedBy: .Equal, toItem: centerredView, attribute: .CenterY, multiplier: 1.0, constant: 0.0)
+            view.addConstraint(yCoordinate)
+            
+            return centerredButton
         }
 
     }
