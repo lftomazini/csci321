514d1513843259803d8b8bbc679240c4b5c320a2
Luís Felipe <lftomazini@wifi177-038.bucknell.edu>
2016-02-18 16:27:01 -0500

    Exercise complete

--- PictureBrowser/PictureBrowser/PictureBrowser/ViewController.swift
+++ PictureBrowser/PictureBrowser/PictureBrowser/ViewController.swift
@@ -17,26 +17,60 @@ class ViewController: UIViewController, UICollectionViewDataSource {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        createFileList()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func createFileList(){
+        let thumbnailPath = thumbnailDirectoryPath()
+        let defaultFileManager = NSFileManager.defaultManager()
+        do {
+            let thumbnailList = try defaultFileManager.contentsOfDirectoryAtPath(thumbnailPath)
+            for fileName in thumbnailList {
+                fileList.append(fileName)
+            }
+        }
+        catch let error as NSError {
+            print("\(__FUNCTION__): \(error.description)")
+        }
+    }
+    
+    /// Finds a path to the thumbnail directory. The thumbnail directory is 
+    /// a subdirectory of the app's resource path.
+    ///
+    /// - returns: the path to the thumbnail directory
+    ///
+    func thumbnailDirectoryPath() -> String {
+        return resourcePath().stringByAppendingPathComponent(thumbnailDirectory)
+    }
 
-
+    /// Returns the path to the resource directory. Note: This method returns 
+    /// an NSString because NSString has the method stringByAppendingPathComponent 
+    /// which is useful when constructing a file path
+    func resourcePath() -> NSString {
+        return NSBundle.mainBundle().resourcePath!
+    }
 }
 
 // MARK: - UICollectionViewDataSource
 
 extension ViewController {
     func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        print("Reporting the number of items in ection")
-        return 0;
+        return fileList.count;
     }
     
     func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
-        return UICollectionViewCell()
+        let cell = thumbnailCollection.dequeueReusableCellWithReuseIdentifier("Thumbnail", forIndexPath: indexPath) as! ThumbnailCollectionViewCell
+        let fileName = (thumbnailDirectoryPath() as NSString).stringByAppendingPathComponent(fileList[indexPath.row])
+        guard let image = UIImage(named: fileName) else {
+            print("Image is nil for index \(indexPath.row)")
+            return cell
+        }
+        cell.thumbnailView.image = image
+        return cell
     }
 }
