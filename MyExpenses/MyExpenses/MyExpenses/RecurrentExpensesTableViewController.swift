ef79efbd88a3bbfb7316794cdeb7eeab87362fc7
Luís Felipe <lftomazini@wifi177-038.bucknell.edu>
2016-04-12 23:03:37 -0400

    using stack views

---
+++ MyExpenses/MyExpenses/MyExpenses/RecurrentExpensesTableViewController.swift
@@ -0,0 +1,47 @@
+//
+//  RecurrentExpensesTableViewController.swift
+//  MyExpenses
+//
+//  Created by Luís Felipe on 4/11/16.
+//  Copyright © 2016 Luís Felipe. All rights reserved.
+//
+
+import UIKit
+
+class RecurrentExpensesTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+    /*
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+}
